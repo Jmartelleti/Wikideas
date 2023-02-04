572298e7e4792f939fe3bdfce53572698b8dd578
Dgagliano <diegogagliano2015@gmail.com>
2023-02-04 14:26:27 -0300

    new file:   aplicacion/.gitignore 	new file:   aplicacion/README.md 	new file:   aplicacion/package-lock.json 	new file:   aplicacion/package.json 	new file:   aplicacion/public/favicon.ico 	new file:   aplicacion/public/index.html 	new file:   aplicacion/public/logo192.png 	new file:   aplicacion/public/logo512.png 	new file:   aplicacion/public/manifest.json 	new file:   aplicacion/public/robots.txt 	new file:   aplicacion/src/App.css 	new file:   aplicacion/src/App.js 	new file:   aplicacion/src/App.test.js 	new file:   aplicacion/src/components/miAplication.js 	new file:   aplicacion/src/index.css 	new file:   aplicacion/src/index.js 	new file:   aplicacion/src/logo.svg 	new file:   aplicacion/src/reportWebVitals.js 	new file:   aplicacion/src/setupTests.js

---
+++ aplicacion/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import miAplication from './components/miAplication';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WikiIdeas</h1>
+      <miAplication />   
+    </div>
+  );
+}
+
+export default App;
---
+++ aplicacion/src/components/miAplication.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const miAplication = () => {
+  return (
+    <div>miAplication</div>
+  )
+}
+
+export default miAplication
