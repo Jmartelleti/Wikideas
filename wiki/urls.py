209d6227fa6c16d15ed03a60e6ed7041d4300743
juan cruz martelleti <juanmartelleti@gmail.com>
2023-02-04 17:18:14 -0300

    modified:   wiki/urls.py

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .import views
 
 urlpatterns = [
-    
+    path('get/',views.getWIKI),
+    path('post/',views.postWIKI),
+    path('put/<int:pk/>',views.putWIKI),
+    path('put/<int:pk/>',views.deleteWIKI),
 ]
