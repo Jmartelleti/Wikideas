7a40c345cdb8ebef622c1514e7c9e717d8b28764
juan cruz martelleti <juanmartelleti@gmail.com>
2023-02-04 17:03:31 -0300

    modified:   wiki/views.py

--- wiki/views.py
+++ wiki/views.py
@@ -7,8 +7,8 @@ from .serializers import Wikiideas #importacion de class
 
 @api_view(['GET'])
 def getWIKI(request):
-    Wiki = Wiki,objects.all()
-    serializer = Wikiideas(wiki, many=True)
+    Wiki = Wiki.objects.all()
+    serializer = Wikiideas(Wiki, many=True)
     return Response (serializer.data)
 
 @api_view(['POST'])
@@ -16,4 +16,21 @@ def postWIKI(request):
     data=request.data
     Wiki = Wiki.objects.create(
         body=data['body']
-    )    
+    )
+    serializer = Wikiideas(Wiki , many= False)
+    return Response(serializer.data)
+
+api_view(['PUT'])
+def putWIKI(request,pk):
+    data = request.data 
+    wiki = Wiki.objects.get(id=pk)
+    serializer = Wikiideas(instance = Wiki , data= data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+api_view(['DELETE'])
+def deleteWIKI(request,pk):
+    Wiki = Wiki.objects.get(id=pk)
+    Wiki.delete()
+    return Response('Wiki eliminado ')
