a188be34dcf5f1004b33670cd606acfb71fd74d6
juan cruz martelleti <juanmartelleti@gmail.com>
2023-02-18 11:07:31 -0300

    new file:   wiki/templates/elements.html     new file:   wiki/templates/generic.html 	modified:   wiki/templates/index.html 	modified:   wikideas/__pycache__/settings.cpython-310.pyc 	modified:   wikideas/settings.py

--- wikideas/settings.py
+++ wikideas/settings.py
@@ -111,10 +111,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
