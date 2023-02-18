aea42f185f04c25f2019472245405c5a3c0fe9cd
juan cruz martelleti <juanmartelleti@gmail.com>
2023-02-18 11:22:42 -0300

    deleted:    static/css/bootstrap.css 	deleted:    static/css/bootstrap.min.css 	deleted:    static/css/estilos.css 	new file:   static/css/fontawesome-all.min.css 	new file:   static/css/main.css 	new file:   static/css/noscript.css 	new file:   static/images/overlay.png 	deleted:    static/img/01.jpg 	deleted:    static/img/01.png 	deleted:    static/img/02.jpg 	deleted:    static/img/02.png 	deleted:    static/img/03.jpg 	deleted:    static/img/03.png 	deleted:    static/img/04.png 	deleted:    static/img/05.png 	deleted:    static/img/06.png 	deleted:    static/img/07.png 	deleted:    static/img/08.png 	deleted:    static/img/amarillo.jpg 	deleted:    static/img/chica.jpg 	deleted:    static/js/bootstrap.bundle.js 	deleted:    static/js/bootstrap.bundle.min.js 	new file:   static/js/breakpoints.min.js 	new file:   static/js/browser.min.js 	new file:   static/js/jquery.min.js 	new file:   static/js/jquery.scrollex.min.js 	new file:   static/js/jquery.scrolly.min.js 	new file:   static/js/main.js 	new file:   static/js/util.js 	new file:   static/sass/base/_page.scss 	new file:   static/sass/base/_reset.scss 	new file:   static/sass/base/_typography.scss 	new file:   static/sass/components/_actions.scss 	new file:   static/sass/components/_box.scss 	new file:   static/sass/components/_button.scss 	new file:   static/sass/components/_features.scss 	new file:   static/sass/components/_form.scss 	new file:   static/sass/components/_icon.scss 	new file:   static/sass/components/_icons.scss 	new file:   static/sass/components/_image.scss 	new file:   static/sass/components/_list.scss 	new file:   static/sass/components/_row.scss 	new file:   static/sass/components/_section.scss 	new file:   static/sass/components/_spotlight.scss 	new file:   static/sass/components/_statistics.scss 	new file:   static/sass/components/_table.scss     new file:   static/sass/layout/_footer.scss 	new file:   static/sass/layout/_header.scss     new file:   static/sass/layout/_main.scss     new file:   static/sass/layout/_nav.scss 	new file:   static/sass/layout/_wrapper.scss 	new file:   static/sass/libs/_breakpoints.scss 	new file:   static/sass/libs/_functions.scss     new file:   static/sass/libs/_html-grid.scss 	new file:   static/sass/libs/_mixins.scss 	new file:   static/sass/libs/_vars.scss 	new file:   static/sass/libs/_vendor.scss 	new file:   static/sass/main.scss 	new file:   static/sass/noscript.scss 	new file:   static/webfonts/fa-brands-400.eot 	new file:   static/webfonts/fa-brands-400.svg 	new file:   static/webfonts/fa-brands-400.ttf 	new file:   static/webfonts/fa-brands-400.woff 	new file:   static/webfonts/fa-brands-400.woff2 	new file:   static/webfonts/fa-regular-400.eot 	new file:   static/webfonts/fa-regular-400.svg 	new file:   static/webfonts/fa-regular-400.ttf 	new file:   static/webfonts/fa-regular-400.woff 	new file:   static/webfonts/fa-regular-400.woff2 	new file:   static/webfonts/fa-solid-900.eot 	new file:   static/webfonts/fa-solid-900.svg 	new file:   static/webfonts/fa-solid-900.ttf 	new file:   static/webfonts/fa-solid-900.woff 	new file:   static/webfonts/fa-solid-900.woff2 	modified:   wiki/templates/index.html 	modified:   wikideas/__pycache__/settings.cpython-310.pyc 	modified:   wikideas/settings.py

--- wikideas/settings.py
+++ wikideas/settings.py
@@ -111,9 +111,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
+STATIC_URL = '/static/'
 
 
 # Default primary key field type
